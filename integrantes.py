4db1d8d5455d5c670bac0445f3570aae00c48c44
NetAcad DEVASC <changeme@example.com>
2024-07-10 00:15:14 +0000

    agregar integrantes.py al repositorio github

---
+++ integrantes.py
@@ -0,0 +1,16 @@
+# integrantes.py
+
+# Lista de nombres y apellidos de los integrantes del grupo
+integrantes = [
+    "Nicolas Acuña",
+    "Jesus Lillo",
+    "Camila Aguirre"
+]
+
+def mostrar_integrantes():
+    print("Integrantes del grupo:")
+    for integrante in integrantes:
+        print(integrante)
+
+if __name__ == "__main__":
+    mostrar_integrantes()
